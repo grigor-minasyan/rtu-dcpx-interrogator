73a611c9a097ad48b32f40aa20b50df809f40175
Grigor Minasyan <grigorminasyan1998@gmail.com>
2020-04-14 11:03:15 -0700

    updated the cron to update the alarms, COS

--- cron.py
+++ cron.py
@@ -12,10 +12,10 @@ RTU_list = []
 
 db_cnx = mysql.connector.connect(user=config.username, password=config.password, database='project6db')
 db_cursor = db_cnx.cursor(buffered=True)
-db_cursor.execute("SELECT * FROM rtu_list")
+db_cursor.execute("SELECT rtu_id, rtu_ip, rtu_port FROM rtu_list")
 
 # getting the rtu-s to process and putting then in the list
-for id, ip, port, type in db_cursor:
+for id, ip, port in db_cursor:
     RTU_list.append(RTU_data(id=id, ip=ip, port=port))
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -73,12 +73,16 @@ if __name__ == '__main__':
         else:
             db_cursor.execute("UPDATE standing_alarms SET value = %s WHERE rtu_id_display = %s", (rtu.current_data.temp, str(rtu.id) + "_1"))
 
-
         db_cursor.execute("SELECT COUNT(rtu_id_display) FROM standing_alarms WHERE rtu_id_display = %s", (str(rtu.id) + "_2", )) #selecting the hum display
         if db_cursor.fetchone()[0] == 0: # insert the alarm into the table if doesnt exist
             db_cursor.execute("INSERT INTO standing_alarms(rtu_id_display,rtu_id,display,value,mj_und_val,mn_und_val,mn_ovr_val,mj_ovr_val) VALUES (%s, %s, %s, %s, 5, 10, 20, 50)", (str(rtu.id) + "_2", rtu.id, 2, rtu.current_data.hum))
         else:
             db_cursor.execute("UPDATE standing_alarms SET value = %s WHERE rtu_id_display = %s", (rtu.current_data.hum, str(rtu.id) + "_2"))
+
+
+    db_cursor_before = db_cnx.cursor(buffered=True)
+    db_cursor_before.execute("SELECT rtu_id, display, mj_und_set, mn_und_set, mn_ovr_set, mj_ovr_set FROM standing_alarms")
+    list_before = db_cursor_before.fetchall()
 
     db_cursor.execute("UPDATE standing_alarms SET mj_und_set = 0, mn_und_set = 0, mn_ovr_set = 0, mj_ovr_set = 0")
     db_cursor.execute("UPDATE standing_alarms SET mj_und_set = 1 WHERE value < mj_und_val")
@@ -86,9 +90,12 @@ if __name__ == '__main__':
     db_cursor.execute("UPDATE standing_alarms SET mn_ovr_set = 1 WHERE value > mn_ovr_val")
     db_cursor.execute("UPDATE standing_alarms SET mj_ovr_set = 1 WHERE value > mj_ovr_val")
 
-        # list = []
-        # list.append(db_cursor.fetchone())
-        # print(list)
+    db_cursor.execute("SELECT rtu_id, display, mj_und_set, mn_und_set, mn_ovr_set, mj_ovr_set FROM standing_alarms")
+    list_after = db_cursor.fetchall()
+
+    for i in range(len(list_before)):
+        if list_before[i] != list_after[i]: # if chere is a change of state
+            db_cursor.execute("INSERT INTO event_history(type, rtu_id, display) VALUES ('COS', %s, %s)", (list_before[i][0], list_before[i][1]))
 
     db_cnx.commit()
     db_cnx.close()
